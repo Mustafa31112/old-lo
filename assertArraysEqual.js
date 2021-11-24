49c6c8d964f3d4ad1f5c4e33451fa83c838e1095
Mustafa <mustafa31112@gmail.com>
2021-11-24 21:29:56 +0000

    implemented middle

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -19,14 +19,55 @@
 //   return true;
 // };
 
-const eqArrays = require("./eqArrays");
+
+
+const eqArrays = function (a, b) {
+  if (a.length !== b.length) {
+    return false 
+  }
+  for (let i = 0; i < a.length; i++) {
+   if (a[i] !== b[i]) {
+     return false
+   }
+
+  }
+return true;
+}
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]))// => false
+
+
+// const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
-  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+    } else {
+      console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+    }
+ 
+   }
+// if (eqArrays(array1, array2)) {
+  //   console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+  // } else {
+  //   console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+  // }
 };
 
 module.exports = assertArraysEqual;
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+
+assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3])// => false
---
+++ middle.js
@@ -0,0 +1,37 @@
+// const eqArrays = require("./eqArrays");
+
+// const assertArraysEqual = function(array1, array2) {
+//   if (eqArrays(array1, array2)) {
+//     console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+//   } else {
+//     console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+//   }
+// };
+// module.exports = assertArraysEqual;
+
+const middle = function(array) {
+  if (array.length === 1 || array.length === 2 ) {
+    return [];
+  } 
+  if (array.length % 2 === 0 && array.length > 3) {
+return [array[Math.ceil(array.length / 2) - 1], array[Math.ceil(array.length / 2)]];
+} else {
+    let middleIndex = (array.length - 1) / 2;  
+    return [array[middleIndex]]
+    
+  }
+}
+//Math.ceil
+//addd one to middle index 
+
+
+
+
+console.log(middle([1])) // => []
+console.log(middle([1, 2])) // => []
+
+console.log(middle([1, 2, 3])) // => [2]
+console.log(middle([1, 2, 3, 4, 5])) // => [3]
+
+console.log(middle([1, 2, 3, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
