176ca27e53bf3a2c2555c529f165cbb0743acb95
Mustafa <mustafa31112@gmail.com>
2021-11-25 03:32:08 +0000

    implemented countLetters

---
+++ countLetters.js
@@ -0,0 +1,46 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    }else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+    }
+  };
+
+  // const countLetters = function(stringtoCount,) {
+  //   const objectResult = {};
+  //   for (constit of stringtoCount) {
+  //     if  (item !== " ") {
+  //       if (objectResult(item) === undefined) {
+  //         objectResult(item) = 1;
+  //       }else {
+  //         objectResult(item) += 1;
+  //       }
+  //     }
+  //   } return objectResult;
+  // };assertEqual(countLetters('LHL'), {});
+  // const arg = process.argv[2];
+  // console.log(countLetters(arg));
+
+  const countLetters = function(inputString) {
+    // Declare an empty object
+    const countResults = {};
+  
+    // Loop through the string
+    for (const input of inputString) {
+      if (countResults[input]) {
+        countResults[input] += 1;
+      } else {
+        if (input !== " ") {
+          countResults[input] = 1;
+        }
+      }
+    }
+    return countResults;
+  };
+  
+  console.log(countLetters("L H L"));
+  console.log(countLetters("lighthouse in the house"));
+  console.log(countLetters("   "));
+  
+  console.log(assertEqual((countLetters("LHL"))["L"], 2));
+  console.log(assertEqual((countLetters("LHL"))["H"], 1));
