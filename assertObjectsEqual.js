6eee2bfe90237145618de8bdfcb49c2f762db63f
Mustafa <mustafa31112@gmail.com>
2021-12-02 18:20:19 +0000

    implemented assertobjectsequal function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -16,16 +16,20 @@ return true;
 
 // FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
-  // Implement me!
   const inspect = require('util').inspect;
-  if (!eqObjects(actual, expected))
-   return (console.log(`❌❌❌ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`));
-  return (console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`));
-;
-
-
-
-
+  if (eqObjects(actual, expected)) 
+  return (console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`));
+  return (console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`))
 };
 
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), true); // => false
 
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
