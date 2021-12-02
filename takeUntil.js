940e12c32cff09316dd0faad4656357374c01e5c
Mustafa <mustafa31112@gmail.com>
2021-12-02 17:18:56 +0000

    implemmented takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,31 @@
+// const eqArrays = require("./eqArrays"); //also can be used 
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+
+};
+const takeUntil = function(array, callback) {
+let newArray = [];
+for (let element of array) {
+  if (!callback(element)) {
+    newArray.push(element);
+  } else {
+    break;
+  }
+}
+return newArray;
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
